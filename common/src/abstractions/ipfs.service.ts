e9576a5be3f45d2fa0c6cbcf174d5a44b757a324
qsobad <qsobad@gmail.com>
2021-09-25 09:58:37 +0800

    added provider and web3.storage

--- common/src/abstractions/ipfs.service.ts
+++ common/src/abstractions/ipfs.service.ts
@@ -1,4 +1,4 @@
 
 export abstract class IpfsService {
-    test: () => any;
+    upload: () => any;
 }
--- common/src/services/ipfs.service.ts
+++ common/src/services/ipfs.service.ts
@@ -5,25 +5,51 @@ import { IpfsService as IpfsServiceAbstraction } from '../abstractions/ipfs.serv
 export class IpfsService implements IpfsServiceAbstraction {
     private token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDNkRjQzZjJmNmVFQmVmZmMzNEEzYTk0NzRGRDk1OEFDMTQ3ZjdiMmUiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2MzIyMDY3NTc5MTUsIm5hbWUiOiJ0aWFuY2hpIn0.RqVsEe2aYLUkm5kdqW7tiTAdEj_i0uv854BuITXXm7Q';
     private storage: Web3Storage;
+    private provider: any;
+    private account : string;
 
     constructor() {
         this.storage = new Web3Storage({ token: this.token });
+
+        const createMetaMaskProvider = require('metamask-extension-provider');
+        this.provider = createMetaMaskProvider();
+
     }
 
-    init() {
+    async init() {
         console.log('ipfs');
+
+        if(this.provider.isMetaMask) {
+            const accounts = await this.provider.request({ method: 'eth_requestAccounts' });
+
+            if (this.provider.isConnected()){
+                this.account = accounts[0];
+                console.log('metamask connected:', this.account);
+            }
+
+        }
     }
 
-    test () {
+    async upload () {
         if (!this.token) {
             return console.error('A token is needed. You can create one on https://web3.storage');
         }
 
-        const files = [];
+        const files = [
+            this.makeFileObject('file.json', '{"hello": "world"}')
+        ];
 
         console.log(`Uploading ${files.length} files`);
-        //const cid = await this.storage.put(files);
-        //console.log('Content added with CID:', cid);
+        const cid = await this.storage.put(files);
+        console.log('Content added with CID:', cid);
     }
+
+    makeFileObject (objName: string, objString: string) {
+        const blob = new Blob([objString], {type : 'application/json'});
+
+        const file = new File([blob], objName);
+        return file;
+    }
+
 
 }
